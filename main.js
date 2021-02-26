df42c6d8a84573e9b019937540fef0565e9d0299
albertomfe <albertomfe@gmail.com>
2021-02-26 00:34:27 -0600

    funcion de salto

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ document.addEventListener('keydown',function(evento){
   /*saltar*/
   if(evento.keyCode==32)
   {
-    console.log('salta');
+    //console.log('salta');
+    saltar();
   }
 
 });
@@ -41,7 +42,7 @@ function borrarCanvas(){
 var imgRex,imgNube,imgCactus,imgSuelo;
 function cargarImagenes(){
   imgRex=new Image();//crear objeto imagen
-  imgRex.src='img/rex.png';
+  imgRex.src='./img/rex.png';
 
   imgNube=new Image();//crear objeto imagen
   imgNube.src='img/nube.png';
@@ -53,16 +54,49 @@ function cargarImagenes(){
   imgSuelo.src='img/suelo.png';
 }
 
+var trex={y:90, vy:0,gravedad:2,salto:24,vymax:9,saltando:false};//objeto rex
 function dibujaRex()
 {
+  //console.log('dibujando');
   cargarImagenes();//crear objetos imagen
-  /*,,w,h,x,y*/
-  ctx.drawImage(imgRex,0,0,44,47,100,100,50,50);
+  /*,,x,y,w,h*/
+  ctx.drawImage(imgRex,0,0,256,256,50,trex.y,100,100);
 }
+
+/**
+*efecto de gravedad
+*/
+function gravedad(){
+//si esta saltando se aplica la gravedad
+ if(trex.saltando==true){
+
+     if(trex.y>90)//no puede pasar la posicion del suelo
+     {
+       //inicializar
+       trex.saltando=false;
+       trex.vy=0;
+       trex.y=90;
+     }
+     else{
+       //aplicar gravedad si no esta en el suelo
+       trex.vy=trex.vy-trex.gravedad;
+       trex.y=trex.y-trex.vy;
+     }
+ }
+}
+
+/**
+*funcion de salto
+*/
+function saltar(){
+  trex.saltando=true;
+  trex.vy=trex.salto;
+}
+
 
 //----------------------------------------------------
 //BUBLE PRINCIPAL 10 veces por segundo
-var FPS=10;//frapes x segundo
+var FPS=50;//frapes x segundo
 //empezar a ciclar despues de cargar html y dom
 setInterval(function()
 {
@@ -78,5 +112,6 @@ setInterval(function()
 function principal(){
   console.log('principal');
   borrarCanvas();
+   gravedad();
   dibujaRex();//dibujar el dinosaurio
 }
